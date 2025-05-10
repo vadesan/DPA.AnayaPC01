8d766b2556a88d71123e4257978f5dd6a943ca5d
Victor Anaya <victor@landgorilla.com>
2025-05-10 17:57:16 -0500

    Mejorar configuración de Entity Framework y base de datos
    Se ha añadido la referencia a `Microsoft.EntityFrameworkCore` en `Program.cs` para permitir el uso de Entity Framework Core en la aplicación. Se ha eliminado la variable `_config` y se ha reemplazado por `connectionString`, simplificando el acceso a la cadena de conexión. Además, se ha configurado el contexto de la base de datos `ReservasdbContext` para utilizar SQL Server, facilitando la interacción con la base de datos. También se han añadido preguntas y respuestas en `preguntas.txt` relacionadas con la experiencia de desarrollo y el uso de herramientas de IA.

--- DPA.AnayaPC01.API/Program.cs
+++ DPA.AnayaPC01.API/Program.cs
@@ -1,9 +1,13 @@
 using DPA.AnayaPC01.DOMAIN.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+var _config = builder.Configuration;
+var connectionString = _config.GetConnectionString("DeveloperConnection");
+builder.Services.AddDbContext<ReservasdbContext>(options =>
+    options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
